2c4f288f1690f0c2305dfeb2484a4a085fafc7aa
Alex Maldonado <alex@oasci.org>
2023-12-11 16:39:02 -0500

    Merge __init__ to module name

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -1,5 +1,6 @@
 """Generate the code reference pages and navigation."""
 
+import os
 from pathlib import Path
 
 import mkdocs_gen_files
@@ -10,11 +11,12 @@ write_dir = "api"
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path(write_dir, doc_path)
+    full_doc_path = os.path.join(write_dir, doc_path)
     parts = tuple(module_path.parts)
 
     if parts[-1] == "__init__":
         parts = parts[:-1]
+        full_doc_path = full_doc_path.replace("__init__.md", "index.md")
     elif parts[-1] == "__main__":
         continue
 
