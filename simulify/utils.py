84644303e0de8d43d7ae753e1cb37c110ff155b5
Alex Maldonado <alex@oasci.org>
2023-12-10 23:50:29 -0500

    Add get_obj_from_string

---
+++ simulify/utils.py
@@ -0,0 +1,19 @@
+import importlib
+
+from loguru import logger
+
+def get_obj_from_string(import_string: str) -> object:
+    """Retrieves an object based on an import string and object name.
+
+    Args:
+        import_string: The import string, starting from the root module, containing
+            the desired object.
+
+    Returns:
+        The object identified by the import string.
+    """
+    logger.debug("Importing {}", import_string)
+    module_name, obj_name = import_string.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    obj = getattr(module, obj_name)
+    return obj
