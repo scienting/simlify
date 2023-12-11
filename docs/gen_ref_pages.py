420cb7460b4e45bda0f4eddd4f5fad938ab5af7e
Alex Maldonado <alex@oasci.org>
2023-12-11 16:00:44 -0500

    Cleanup gen_ref

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -1,26 +1,19 @@
-"""Generate the code reference pages."""
+"""Generate the code reference pages and navigation."""
 
-import os
 from pathlib import Path
 
 import mkdocs_gen_files
 
-SRC_DIR = "simulify"
-WRITE_DIR = "api"
+src = Path(__file__).parent.parent / "simulify"
+write_dir = "api"
 
-for path in sorted(Path(SRC_DIR).rglob("*.py")):  #
-    module_path = path.relative_to(SRC_DIR).with_suffix("")  #
+for path in sorted(src.rglob("*.py")):
+    module_path = path.relative_to(src).with_suffix("")
+    doc_path = path.relative_to(src).with_suffix(".md")
+    full_doc_path = Path(write_dir, doc_path)
+    parts = tuple(module_path.parts)
 
-    doc_path = path.relative_to(SRC_DIR).with_suffix(".md")  #
-
-    if not os.path.exists(Path(WRITE_DIR)):
-        os.mkdir(Path(WRITE_DIR))
-
-    full_doc_path = Path(WRITE_DIR, doc_path)  #
-
-    parts = list(module_path.parts)
-
-    if parts[-1] == "__init__":  #
+    if parts[-1] == "__init__":
         parts = parts[:-1]
     elif parts[-1] == "__main__":
         continue
@@ -28,9 +21,8 @@ for path in sorted(Path(SRC_DIR).rglob("*.py")):  #
     if len(parts) == 0:
         continue
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  #
-        identifier = ".".join(parts)  #
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
 
-        print("::: " + identifier, file=fd)  #
-
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)  #
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
