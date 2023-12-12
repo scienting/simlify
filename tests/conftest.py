3b693aa2559c5fef09b0d13aed12d585f0fd2145
Alex Maldonado <alex@oasci.org>
2023-12-11 20:27:33 -0500

    Add some pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,94 @@ import os
 import pytest
 
 from simlify import enable_logging
+from simlify.simulation.contexts import SimContextManager
+from simlify.simulation.packages.amber.contexts import AMBER_PROTEIN_STANDARD_CONTEXT
 
 TEST_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def test_dir():
+    return os.path.abspath(TEST_DIR)
 
 
 @pytest.fixture(scope="session", autouse=True)
 def turn_on_logging():
     enable_logging(10)
+
+
+@pytest.fixture
+def path_1jc0():
+    return os.path.join(TEST_DIR, "files/structures/1JC0.pdb")
+
+
+@pytest.fixture
+def path_1jc0_prepped():
+    return os.path.join(TEST_DIR, "files/structures/1JC0-prepped.pdb")
+
+
+@pytest.fixture
+def path_cro_fcrmod():
+    return os.path.join(
+        TEST_DIR, "files/ff/amber-ff-chromo-params/frcmod.xFPchromophores.2022"
+    )
+
+
+@pytest.fixture
+def path_cro_lib():
+    return os.path.join(
+        TEST_DIR, "files/ff/amber-ff-chromo-params/xFPchromophores.lib.2022"
+    )
+
+
+@pytest.fixture
+def amber_protein_standard_context():
+    context_manager = SimContextManager(**AMBER_PROTEIN_STANDARD_CONTEXT)
+    return context_manager
+
+
+@pytest.fixture
+def amber_simulation_standard_context(amber_protein_standard_context):
+    context_manager = amber_protein_standard_context
+    context_manager.name_stage = "01_min"
+    context_manager.dir_input = TEST_DIR
+    context_manager.dir_output = TEST_DIR
+    context_manager.path_topo = os.path.join(TEST_DIR, "mol.prmtop")
+    context_manager.path_input = os.path.join(
+        TEST_DIR, f"{context_manager.name_stage}.in"
+    )
+    context_manager.path_restart_prev = os.path.join(TEST_DIR, "mol.inpcrd")
+    context_manager.path_coord_ref = os.path.join(TEST_DIR, "mol.inpcrd")
+    context_manager.compute_platform = "mpi"
+    context_manager.cpu_cores = 12
+    context_manager.input_kwargs = {
+        "irest": 1,
+        "ntx": 5,
+        "ig": -1,
+        "dt": 0.002,
+        "nstlim": 500000,
+        "nscm": 500,
+        "ntr": 1,
+        "restraint_wt": 0.5,
+        "restraintmask": "!(:WAT) & (@C,CA,N,O,O5',P,O3',C3',C4',C5')",
+        "ntb": 2,
+        "ntf": 2,
+        "ntc": 2,
+        "cut": 10.0,
+        "ntt": 3,
+        "temp0": 300.0,
+        "gamma_ln": 5.0,
+        "ntp": 1,
+        "barostat": 2,
+        "pres0": 1.01325,
+        "mcbarint": 100,
+        "comp": 44.6,
+        "taup": 1.0,
+        "ntxo": 2,
+        "ntwr": 5000,
+        "ntpr": 500,
+        "ntwx": 5000,
+        "ioutfm": 1,
+        "iwrap": 1,
+    }
+    return context_manager
