df2741abfb86a861ca43d2a2923d0f3881f89eac
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-12-13 00:56:33 -0500

    Start data definitions

---
+++ simlify/data/atomistic.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from pint import Quantity
+
+from .. import ureg
+
+
+@dataclass
+class Positions:
+    """Cartesian coordinates of a physical system."""
+
+    dtype: str = "<f8"
+
+    _units: Quantity = 1.0 * ureg.angstrom
+
+    @property
+    def units(self):
+        """Default: Angstrom"""
+        return self._units
+
+    @units.setter
+    def units(self, new_unit: Quantity) -> None:
+        self._units *= new_unit
