f88c53703cdd776e14278a60f22a408c9ddfc403
Alex Maldonado <alex@oasci.org>
2024-06-13 20:28:50 -0400

    refactor: remove leftover amber schema

--- tests/test_simulation_contexts.py
+++
@@ -1,7 +0,0 @@
-from simlify.simulation.amber.contexts import AmberContextValidator
-
-
-class TestValidateContexts:
-    def test_amber_protein_standard_context(self, amber_protein_standard_context):
-        """Test if the standard amber protein simulation context is valid."""
-        AmberContextValidator.validate(amber_protein_standard_context)
